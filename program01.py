1b91c3360c96b78e064fd12591aade61c7599811
Parth Gosavi <83508546+parth01032000@users.noreply.github.com>
2022-10-11 16:10:30 +0530

    Add files via upload

---
+++ program01.py
@@ -0,0 +1,31 @@
+# program to print hello world
+a="hello world!"
+print(a)
+
+# Write a Python program to do arithmetical operations addition and division.
+b=4
+c=5
+d=b+c
+e=b/c
+print(d)
+print(e)
+
+# Write a Python program to find the area of a triangle
+base=4
+height=6
+area=0.5*base*height
+print(area)
+
+# Write a Python program to swap two variables
+a=4
+b=5
+print(f"value of a:{a} and b:{b} before swaping")
+temp=a
+a=b
+b=temp
+print(f"value of a:{a} and b:{b} after swaping")
+
+# Write a Python program to generate a random number?
+import random
+
+print(random.randint(0,100))
